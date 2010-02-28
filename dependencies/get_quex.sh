2cc87552e65bf533d4968c53829130570ab83709
Dave Cunningham <sparkprime@gmail.com>
2010-02-28 08:09:25 +0000

    Patches to help get running

--- dependencies/get_quex.sh
+++ dependencies/get_quex.sh
@@ -2,9 +2,13 @@
 
 source get_common.sh
 
-QUEX='quex-0.34.1.tar.gz'
+QUEX_DIR='quex-0.34.1'
+QUEX="${QUEX_DIR}.tar.gz"
 
 echo "Downloading quex from soureforge via http..."
 $WGET "http://downloads.sourceforge.net/quex/${QUEX}?modtime=1228587839&big_mirror=0"
 echo "Unpacking quex..."
 $TAR -xzf "$QUEX" && $RM "$QUEX"
+
+do_patch "${QUEX_DIR}" "quex.patch"
+
