00d01797e92ec4cf736ef3b406866723ad884373
Dave Cunningham <sparkprime@gmail.com>
2010-06-20 01:05:30 +0000

    Update to latest bullet, put quex into repository

--- dependencies/get_icu_win32_prebuilt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source get_common.sh
-
-ICU="icu4c-4_2_1-Win32-msvc9.zip"
-
-echo "Downloading libicu from its website via http..."
-$WGET "http://download.icu-project.org/files/icu4c/4.2.1/$ICU"
-$UNZIP "$ICU" && $RM "$ICU"
