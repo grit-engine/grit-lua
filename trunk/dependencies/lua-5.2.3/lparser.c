ea35fa6ea0beccc9f60b4961ab36271bc1deaa9f
Dave Cunningham <sparkprime@gmail.com>
2014-04-07 20:03:08 +0000

    Add r implementation, remove fqn, fqn_ex, and equivalent path_util functionality.  Modify all scripts to use new resource directory system, and r where necessary.  Disable tail call optimizations in Lua.  Windows & Linux builds.

--- trunk/dependencies/lua-5.2.3/lparser.c
+++ trunk/dependencies/lua-5.2.3/lparser.c
@@ -1504,7 +1504,7 @@ static void retstat (LexState *ls) {
     nret = explist(ls, &e);  /* optional return values */
     if (hasmultret(e.k)) {
       luaK_setmultret(fs, &e);
-      if (e.k == VCALL && nret == 1) {  /* tail call? */
+      if (e.k == VCALL && nret == 1 && 0) {  /* tail call? */
         SET_OPCODE(getcode(fs,&e), OP_TAILCALL);
         lua_assert(GETARG_A(getcode(fs,&e)) == fs->nactvar);
       }
